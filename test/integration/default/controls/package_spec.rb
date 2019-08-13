4f75eac5fec80fc5bf491b6504baf3e6940df764
sticky-note <nb@nowhere.nw>
2019-08-14 01:33:57 +1100

    fix(tests): fix package name for debian

--- test/integration/default/controls/package_spec.rb
+++ test/integration/default/controls/package_spec.rb
@@ -3,7 +3,14 @@ control 'Php package' do
 
   pkg_name =
     case os[:family]
-    when 'debian', 'redhat', 'fedora'
+    when 'debian'
+      case os[:name]
+      when 'ubuntu'
+        'php'
+      when 'debian'
+        'php7.0'
+      end
+    when 'redhat', 'fedora'
       'php'
     when 'suse'
       'php5'
