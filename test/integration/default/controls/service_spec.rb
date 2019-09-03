baeac04bd9bb691b5a99f44a379018c59d9f7060
Nicolas Rodriguez <nicoladmin@free.fr>
2019-09-04 01:12:38 +0200

    test(service): add tests on Php services

---
+++ test/integration/default/controls/service_spec.rb
@@ -0,0 +1,50 @@
+control 'Php service' do
+  title 'should be running and enabled'
+
+  def test_debian
+    describe service('php5.6-fpm') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe service('php7.3-fpm') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+
+  def test_ubuntu
+    describe service(pkg_name) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+
+  def test_redhat
+    describe service('php') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+
+  def test_suse
+    describe service('php5') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+
+  case os[:family]
+  when 'debian'
+    case os[:name]
+    when 'ubuntu'
+      test_ubuntu
+    when 'debian'
+      test_debian
+    end
+  when 'redhat', 'fedora'
+    test_redhat
+  when 'suse'
+    test_suse
+  end
+end
