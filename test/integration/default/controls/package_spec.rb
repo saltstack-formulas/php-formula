59f648c0671a96ee89b858e092713308e7342754
Nicolas Rodriguez <nicoladmin@free.fr>
2019-09-04 01:12:28 +0200

    test(package): fix package spec in case of multi Php versions

--- test/integration/default/controls/package_spec.rb
+++ test/integration/default/controls/package_spec.rb
@@ -1,22 +1,61 @@
 control 'Php package' do
   title 'should be installed'
 
-  pkg_name =
-    case os[:family]
-    when 'debian'
-      case os[:name]
-      when 'ubuntu'
-        'php'
-      when 'debian'
-        'php7.0'
-      end
-    when 'redhat', 'fedora'
-      'php'
-    when 'suse'
-      'php5'
+
+
+  def test_debian
+    describe package('php-imagick') do
+      it { should be_installed }
     end
 
-  describe package(pkg_name) do
-    it { should be_installed }
+    describe package('php-redis') do
+      it { should be_installed }
+    end
+
+    %w[
+      bz2 cli curl fpm gd imap intl mbstring
+      mysql readline xml zip
+    ].each do |pkg|
+
+      describe package("php5.6-#{pkg}") do
+        it { should be_installed }
+      end
+
+      describe package("php7.3-#{pkg}") do
+        it { should be_installed }
+      end
+    end
+  end
+
+  def test_ubuntu
+    describe package(pkg_name) do
+      it { should be_installed }
+    end
+  end
+
+  def test_redhat
+    describe package('php') do
+      it { should be_installed }
+    end
+  end
+
+  def test_suse
+    describe package('php5') do
+      it { should be_installed }
+    end
+  end
+
+  case os[:family]
+  when 'debian'
+    case os[:name]
+    when 'ubuntu'
+      test_ubuntu
+    when 'debian'
+      test_debian
+    end
+  when 'redhat', 'fedora'
+    test_redhat
+  when 'suse'
+    test_suse
   end
 end
